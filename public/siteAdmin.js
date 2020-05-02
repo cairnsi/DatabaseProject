d31e478cd1f8168ec1d440f8d9b04ad68aa49856
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-05-02 18:21:46 -0500

    edit tour types

--- public/siteAdmin.js
+++ public/siteAdmin.js
@@ -1,18 +1,22 @@
 function bindButtons(){
-  document.getElementsByClassName('editTourType')[0].addEventListener('click', function(event){
-	  var req = new XMLHttpRequest();
-	  var path = '/editTourType';
-	  req.open('GET', path, false);
-		req.addEventListener('load',function(){
-			if(req.status >= 200 && req.status < 400){
-				location.href = path;
-			} else {
-				console.log("Error in network request: " + req.statusText);
-				document.getElementById('addMessage').textContent="Could Not Edit";
-				document.getElementById('addMessage').style.color='red';
-			}
-		});
-		req.send();
-  });
+	var tourElements = document.getElementsByClassName('editTourType');
+	var tour;
+	for(tour in tourElements){
+	  tour.addEventListener('click', function(event){
+		  var req = new XMLHttpRequest();
+		  var path = '/editTourType';
+		  req.open('GET', path, false);
+			req.addEventListener('load',function(){
+				if(req.status >= 200 && req.status < 400){
+					location.href = path;
+				} else {
+					console.log("Error in network request: " + req.statusText);
+					document.getElementById('addMessage').textContent="Could Not Edit";
+					document.getElementById('addMessage').style.color='red';
+				}
+			});
+			req.send();
+	  });
+	}
 }
 document.addEventListener('DOMContentLoaded', bindButtons);
