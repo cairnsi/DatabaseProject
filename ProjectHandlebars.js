de47501097049074428dd28b0614c743351a5414
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-05-02 14:03:57 -0500

    added checkout and tour and service page

--- ProjectHandlebars.js
+++ ProjectHandlebars.js
@@ -30,150 +30,22 @@ app.get('/',function(req,res){
    res.render('home',context);
 });
 
-app.get('/editItem',function(req,res){
+app.get('/addTour',function(req,res){
   var context = {};
-  if(req.query.id){
-	if(!req.query.reps)
-		req.query.reps = "";
-	if(!req.query.weight)
-		req.query.weight="";
-	if(!req.query.lbs)
-		req.query.lbs="";
-	if(!req.query.date)
-		req.query.date="";
-	
-	context.name = req.query.name;
-	context.reps = req.query.reps;
-	context.weight = req.query.weight;
-	context.date = req.query.date;
-	context.id = req.query.id;
-	
-	if(req.query.units=="lbs"){
-		context.lbs = "selected";
-	}
-	if(req.query.units=="kg"){
-		context.kg = "selected";
-	}
-	
-	context.id = req.query.id;
-	  
-    res.render('edit',context);
-    return;
-  }
-  res.type('plain/text');
-  res.status(500);
-  res.render('500');
+   res.render('addTour',context);
 });
 
-app.get('/table', function(req,res){
-	pool.query("Select * FROM workouts", function(err,result){ 
-    if(!err){
-		res.send(JSON.stringify(result));
-		
-	}else{
-		next(err);
-	}
-  });
-});
-
-
-app.post('/tableInsert',function(req,res,next){
-	if(req.body.name && req.body.name!=""){
-		if(req.body.reps==""){
-			req.body.reps=null;
-		}
-		if(req.body.weight==""){
-			req.body.weight=null;
-		}
-		pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?)", [[req.body.name,req.body.reps,req.body.weight,req.body.date,req.body.units]], function(err, result){
-			if(err){
-				next(err);
-				return;
-			}else{
-				var data={};
-				res.json(JSON.stringify(result));
-			}
-		});
-	}else{
-		res.type('plain/text');
-		res.status(500);
-		res.render('500');
-	}
-	
-});
-
-app.post('/update',function(req,res,next){
-	if(req.body.id){
-		if(req.body.reps==""){
-			req.body.reps=null;
-		}
-		if(req.body.weight==""){
-			req.body.weight=null;
-		}
-		pool.query("UPDATE workouts SET name = ?, reps= ?, weight= ?, date= ?, lbs = ? WHERE id = ?", [req.body.name,req.body.reps,req.body.weight,req.body.date,req.body.units,req.body.id], function(err, result){
-			if(err){
-				next(err);
-				return;
-			}else{
-				res.json(JSON.stringify(result));
-			}
-		});
-	}else{
-		res.type('plain/text');
-		res.status(500);
-		res.render('500');
-	}
-	
-});
-
-app.post('/tableReset',function(req,res,next){
+app.get('/checkout',function(req,res){
   var context = {};
-  pool.query("DROP TABLE IF EXISTS workouts", function(err){ 
-    if(!err){
-		var createString = "CREATE TABLE workouts("+
-		"id INT PRIMARY KEY AUTO_INCREMENT,"+
-		"name VARCHAR(255) NOT NULL,"+
-		"reps INT,"+
-		"weight INT,"+
-		"date VARCHAR(255),"+
-		"lbs VARCHAR(255))";
-		pool.query(createString, function(err){
-			if(!err){
-				var data={};
-				data.result = "Table reset";
-				res.json(JSON.stringify(data));
-			}else{
-				next(err);
-			}
-		});
-	}else{
-		next(err);
-	}
-  });
+   res.render('checkout',context);
 });
 
-app.post('/tableDelete',function(req,res,next){
-  if(req.body.id){
-		pool.query("DELETE FROM workouts WHERE id = ?", req.body.id, function(err, result){
-			if(err){
-				next(err);
-				return;
-			}else{
-				var data={};
-				res.json(JSON.stringify(result));
-			}
-		});
-	}else{
-		res.type('plain/text');
-		res.status(500);
-		res.render('500');
-	}
-});
-
-app.post('/', function(req,res,next){
+app.get('/addService',function(req,res){
   var context = {};
-   res.render('home',context);
+   res.render('addService',context);
 });
+
+
 
 app.use(function(req,res){
   res.status(404);
