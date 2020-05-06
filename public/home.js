16bebf6534ad35829851b018bfdf0bd858ebf2bd
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-05-05 23:47:39 -0500

    added Service type table from database

--- public/home.js
+++ public/home.js
@@ -42,4 +42,49 @@ function displayTourTypeTable(){
 	  req.send();
 	  
 }
+
+function displayServiceTypeTable(){
+	var req = new XMLHttpRequest();
+	req.open('GET', '/serviceTypesTable', true);
+	  req.setRequestHeader('Content-Type', 'application/json');
+	  req.addEventListener('load',function(){
+		  if(req.status >= 200 && req.status < 400){
+			  document.getElementById('serviceTypesTable').innerHTML="";
+			  var table = document.getElementById('serviceTypesTable');
+			  var response = JSON.parse(req.responseText);
+			  if(response.length<1){
+				var row = table.insertRow();
+				row.innerHTML = "<th>Table is empty</th>"
+				return;
+			  }
+			  
+			  table.innerHTML="";
+			  var row = table.insertRow();
+			  row.innerHTML = "<th>Service Type</th><th>Description</th><th>Cost</th>";
+			  for(var i = 0;i<response.length;i++){
+				  var item = response[i];
+				  row = table.insertRow();
+				  var cell = row.insertCell();
+				  cell.textContent = item.label;
+				  cell = row.insertCell();
+				  cell.textContent = item.description;
+				  cell = row.insertCell();
+				  cell.textContent = '$'+(item.cost/100);
+				  cell.style.width = "15%";
+				  
+				  
+				  cell = row.insertCell();
+				  cell.textContent = item.id;
+				  cell.style.visibility = "hidden";
+				  cell.id = "rowId";
+			  }
+		  }else{
+			  //var row = table.insertRow();
+			  //row.innerHTML = "<th>Did not get data for Table</th>"
+		  }
+	  });
+	  req.send();
+	  
+}
 displayTourTypeTable()
+displayServiceTypeTable()
