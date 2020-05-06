a1443ae0b0fc6a6112d6ef6333b1e60914270a5d
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-05-06 17:26:31 -0500

    adding database init file

--- public/siteAdmin.js
+++ public/siteAdmin.js
@@ -1,3 +1,62 @@
+function displayTourTypeTable(){
+	var req = new XMLHttpRequest();
+	req.open('GET', '/tourTypeTable', true);
+	  req.setRequestHeader('Content-Type', 'application/json');
+	  req.addEventListener('load',function(){
+		  if(req.status >= 200 && req.status < 400){
+			  document.getElementById('toursTypesTable').innerHTML="";
+			  var table = document.getElementById('toursTypesTable');
+			  var response = JSON.parse(req.responseText);
+			  if(response.length<1){
+				var row = table.insertRow();
+				row.innerHTML = "<th>Table is empty</th>";
+				return;
+			  }
+			  
+			  table.innerHTML="";
+			  var row = table.insertRow();
+			  row.innerHTML = "<th>Tour</th><th>Meet Time</th><th>Cost</th>";
+			  for(var i = 0;i<response.length;i++){
+				  var item = response[i];
+				  row = table.insertRow();
+				  var cell = row.insertCell();
+				  cell.textContent = item.label;
+				  cell = row.insertCell();
+				  cell.textContent = item.meet_time;
+				  cell.style.width = "15%";
+				  cell = row.insertCell();
+				  cell.textContent = '$'+(item.cost/100);
+				  cell.style.width = "15%";
+				  
+				  var btn = document.createElement('button');
+				  btn.setAttribute('class', 'editTourType');
+				  btn.innerHTML = 'Edit';
+				  //btn.onclick =  bindEdit(item);
+				  row.appendChild(btn);
+				  
+				  var btn = document.createElement('button');
+				  btn.setAttribute('class', 'removeTourType');
+				  btn.innerHTML = 'Remove';
+				  //btn.onclick =  bindEdit(item);
+				  row.appendChild(btn);
+				  
+				  cell = row.insertCell();
+				  cell.textContent = item.id;
+				  cell.style.visibility = "hidden";
+				  cell.id = "rowId";
+			  }
+		  }else{
+			  //var row = table.insertRow();
+			  //row.innerHTML = "<th>Did not get data for Table</th>"
+		  }
+	  });
+	  req.send();
+	  
+}
+
+
+
+
 function bindButtons(){
 	var tourElements = document.getElementsByClassName('editTourType');
 	for (var i = 0; i < tourElements.length; i++) {
@@ -70,3 +129,4 @@ function bindButtons(){
 	});
 }
 document.addEventListener('DOMContentLoaded', bindButtons);
+displayTourTypeTable();
