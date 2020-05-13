10797730be1af5b777dfbfe3807f66bdc185b91c
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-05-12 22:23:44 -0500

    added session

--- ProjectHandlebars.js
+++ ProjectHandlebars.js
@@ -54,18 +54,18 @@ app.post('/signIn', function(req,res,next){
 			if(checkSession(req,res)){
 				var context = {};
 				context.success = "Successfully signed in."; 
-				res.render('/signIn',context);
+				res.render('signIn',context);
 				return;
 			}else{
 				var context = {};
 				context.error = "Could not set session";
-				res.render('/signIn',context);
+				res.render('signIn',context);
 				return;
 			}
 		}else{
 			var context = {};
 			context.error = "Not a valid user";
-			res.render('/signIn',context);
+			res.render('signIn',context);
 			return;
 		}
 		
