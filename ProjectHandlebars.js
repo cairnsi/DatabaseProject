54fb64797d2c2b9ea9ad2a82231bd6075313866e
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-05-13 22:07:03 -0500

    added session

--- ProjectHandlebars.js
+++ ProjectHandlebars.js
@@ -239,6 +239,39 @@ app.get('/addTourType',function(req,res){
    res.render('addTourType',context);
 });
 
+app.post('/addTourType', function(req,res,next){
+  if(req.body.label&& req.body.meet_time&& req.body.cost){
+	var query = "INSERT INTO Guided_Tour_Types(label, meet_time, cost) VALUES (?)";
+	pool.query(query, [[req.body.label, req.body.meet_time, req.body.cost]],function(err,result){ 
+	  if(!err){
+			console.log(result);
+			var context = {};
+			context.success = "Success";
+			res.render('addTourType',context);
+			return;
+		}else{
+			var context = {};
+			context.error = "Not a valid user";
+			res.render('signIn',context);
+			return;
+		}
+		
+		
+	  }else{
+		next(err);
+	  }
+    });
+	  
+	  
+  }else{
+  
+	var context ={};
+	context.error = "Must enter first and last name";
+	res.render('signIn',context);
+	return
+  }
+});
+
 app.get('/addServiceType',function(req,res){
   var context = {};
    res.render('addServiceType',context);
