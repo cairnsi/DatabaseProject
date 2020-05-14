28025c4779efc8689c64be72bcf548cd938812db
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-05-14 12:52:06 -0500

    edit edit

--- ProjectHandlebars.js
+++ ProjectHandlebars.js
@@ -167,8 +167,9 @@ app.get('/editTourType',function(req,res){
 
 app.post('/editTourType', function(req,res,next){
   if(req.body.id&& req.body.label&& req.body.meet_time&& req.body.cost){
-	var cost = (req.body.cost*100);
-	if(isNaN(cost)){
+	var cost = req.body.cost;
+	if(!isNaN(cost)){
+		cost = cost*100;
 		cost = Math.round(cost);
 		var query = "SELECT id FROM Guided_Tour_Types WHERE label = ?";
 		pool.query(query, [req.body.label],function(err1,result1){ 
