6b3270b1f47b8611745858e3f8eff3e1fa744f0d
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-05-15 11:44:08 -0500

    edit edit

--- public/updateSpecificTours.js
+++ public/updateSpecificTours.js
@@ -106,9 +106,31 @@ function bindAddButton(){
 		req.send();
 	});
 }
+
+function createTourTypeOptions(){
+	var req = new XMLHttpRequest();
+	req.open('GET', '/tourTypeTable', true);
+	  req.setRequestHeader('Content-Type', 'application/json');
+	  req.addEventListener('load',function(){
+		  if(req.status >= 200 && req.status < 400){
+			  var typeSelect = document.getElementById('type');
+			  var response = JSON.parse(req.responseText);
+			  for(var i = 0;i<response.length;i++){
+					var option = document.createElement("option");
+					option.text = item.label;
+					option.value = item.label;
+					x.add(option); 
+			  }
+		  }else{
+			  
+		  }
+	  });
+	  req.send();
+}
 document.addEventListener('DOMContentLoaded', bindAddButton);
 document.addEventListener('DOMContentLoaded', bindFilter);
 document.addEventListener('DOMContentLoaded', bindReset);
 
 var input = {};
 displayToursTable(input);
+createTourTypeOptions();
