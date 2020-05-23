68a61467ebfd4317fdb9a97e25b18ddfb85b3481
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-05-23 14:30:14 -0700

    creating add customer

---
+++ public/addService.js
@@ -0,0 +1,54 @@
+function displayServiceTypeTable(){
+	var req = new XMLHttpRequest();
+	req.open('GET', '/serviceTypesTable', true);
+	  req.setRequestHeader('Content-Type', 'application/json');
+	  req.addEventListener('load',function(){
+		  if(req.status >= 200 && req.status < 400){
+			  document.getElementById('addServiceTable').innerHTML="";
+			  var table = document.getElementById('serviceTypesTable');
+			  var response = JSON.parse(req.responseText);
+			  if(response.length<1){
+				var row = table.insertRow();
+				row.innerHTML = "<th>Table is empty</th>"
+				return;
+			  }
+			  
+			  table.innerHTML="";
+			  var row = table.insertRow();
+			  row.innerHTML = "<th>Service Types</th><th>Description</th><th>Cost</th>";
+			  for(var i = 0;i<response.length;i++){
+				  var item = response[i];
+				  if(!item.active){
+					  continue;
+				  }
+				  row = table.insertRow();
+				  var cell = row.insertCell();
+				  cell.textContent = item.label;
+				  cell = row.insertCell();
+				  cell.textContent = item.description;
+				  cell = row.insertCell();
+				  cell.textContent = '$'+(item.cost/100);
+				  
+				  var btn = document.createElement('button');
+				  btn.setAttribute('class', 'add');
+				  btn.innerHTML = 'Add to Cart';
+				  //btn.onclick =  bindRemoveSpecificTour(item);
+				  row.appendChild(btn);
+				  
+				  var qty = document.createElement("SELECT");
+				  for(var i = 0;i<6;i++){
+					  var item = i;
+					  var option = document.createElement("option");
+					  option.text = i;
+					  option.value = i;
+					  typeSelect.add(option); 
+				  }
+			  }
+		  }else{
+			  
+		  }
+	  });
+	  req.send();
+	  
+}
+displayServiceTypeTable()
